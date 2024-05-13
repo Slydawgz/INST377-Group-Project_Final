9548f58ae937917bb8e80e36c970ec1f562e3b09
Tiara Eltrevoog <tiaraeltrevoog@tiaras-mbp.mynetworksettings.com>
2024-05-13 01:02:25 -0400

    Started About & Home Picture Slider

--- Final Project/Home_js.js
+++ Final Project/Home_js.js
@@ -1,7 +1,8 @@
 function loadAnime() {
-    return fetch(`https://api.jikan.moe/v4/anime/`)
+    return fetch(`https://api.jikan.moe/v4/anime`)
     .then((res) => res.json())
     .then(data => {console.log(data)})
     .catch(error => console.log(error));
 }
+
 window.onload = loadAnime;
